aa31ee16dd26e5da3562fe3408d57e1df3841c00
Espen Moe-Nilssen <espen@medialog.no>
2014-10-14 16:10:32 +0200

    first commit

---
+++ README.txt
@@ -0,0 +1,13 @@
+medialog.Wayforwardtheme
+=======================
+
+A theme for wayforward.no
+
+
+
+Author
+======
+
+Espen Moe-Nilssen, Grieg Medialog AS
+
+
---
+++ medialog/Wayforwardtheme/interfaces.py
@@ -0,0 +1,11 @@
+from zope.interface import Interface
+
+from zope.i18nmessageid import MessageFactory
+
+_ = MessageFactory('medialog.Wayforwardtheme')
+
+
+class IIWayForwardLayer(Interface):
+    """A layer specific to medialog.Wayforwardtheme
+        """
+ 
